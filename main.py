982df94dae63e48fcbdbf0d28134c02415e4356a
Aaron Lip <amitchel.lip@gmail.com>
2021-04-08 18:33:43 -0600

    quantization previews

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 import random
 import math
 import os
+import itertools
 
 class Cluster(object):
     """Store the rolling centroid of a cluster of samples."""
@@ -30,15 +31,26 @@ def kmeans(data, k=3, max_iterations=5):
         print(f'Iteration #{iterations}')
 
         for i, pixel in enumerate(data):
-            nearest = min(clusters, key=lambda cluster: math.sqrt(sum((a - b) ** 2 for a, b in zip(cluster.centroid, pixel))))
+            nearest = min(clusters, key=lambda cluster: euclidean_distance(cluster.centroid, pixel))
             nearest.new_sample(*pixel)
 
         iterations += 1
 
     return [c.centroid for c in clusters]
 
+def euclidean_distance(origin, point):
+    """Calculate the distance between a point and a point of origin."""
+    return math.sqrt(sum((a - b) ** 2 for a, b in zip(origin, point)))
+
+def quantize_image(image, centroids):
+    for x, y in itertools.product(range(image.size[0]), range(image.size[1])):
+        image.putpixel((x, y), tuple(int(i) for i in min(centroids, key=lambda centroid: euclidean_distance(centroid, image.getpixel((x, y))))))
+    image.show()
+
 if __name__ == '__main__':
     import sys
 
     if len(sys.argv) > 2:
-        print(kmeans(Image.open(sys.argv[1]).getdata(), int(sys.argv[2])))
+        with Image.open(sys.argv[1]) as image:
+            centroids = kmeans(image.getdata(), int(sys.argv[2]))
+            quantize_image(image, centroids)
